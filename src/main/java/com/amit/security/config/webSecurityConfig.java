04c8073c3d25807e901d6750d06c781d548eb35a
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-12-20 22:33:33 +0530

    First Commit

---
+++ src/main/java/com/amit/security/config/webSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.amit.security.config;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class webSecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request -> request.anyRequest().authenticated()).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails shabbir = User.withUsername("shabbir").password("{noop}password").roles("User").build();
+
+        UserDetails nikhil = User.withUsername("nikhil").password("{noop}nikhil").roles("User").build();
+
+        return new InMemoryUserDetailsManager(shabbir, nikhil);
+    }
+}
---
+++ src/main/java/com/amit/security/controller/ProductController.java
@@ -0,0 +1,29 @@
+package com.amit.security.controller;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    private record Product(Integer productId, String productName, Double price) {}
+
+    List<Product> products = new ArrayList<>(List.of(
+            new Product(1, "Iphone", 999.0),
+            new Product(2, "Mac Pro", 2099.0)
+    ));
+
+    @GetMapping
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    @PostMapping
+    public Product saveProduct(@RequestBody Product product) {
+        products.add(product);
+        return product;
+    }
+}
