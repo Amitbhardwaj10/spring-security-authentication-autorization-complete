2f792175ed12f6573f52cd20a4ea4aa928905cdd
Amit Bhardwaj <amitbhk2002@gmail.com>
2025-12-22 21:44:04 +0530

    fix the project structure error and implement DAOAuthenticationProvider to authenticate the user from database

--- src/main/java/com/amit/security/config/webSecurityConfig.java
+++ src/main/java/com/amit/security/config/webSecurityConfig.java
@@ -1,8 +1,9 @@
 package com.amit.security.config;
 
-import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -17,18 +19,32 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class webSecurityConfig {
 
+    private final UserDetailsService userDetailsService;
+
+    public webSecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(request -> request.anyRequest().authenticated()).formLogin(Customizer.withDefaults()).httpBasic(Customizer.withDefaults());
         return httpSecurity.build();
     }
 
-    @Bean
+//    @Bean
     public UserDetailsService userDetailsService() {
         UserDetails shabbir = User.withUsername("shabbir").password("{noop}password").roles("User").build();
 
         UserDetails nikhil = User.withUsername("nikhil").password("{noop}nikhil").roles("User").build();
 
         return new InMemoryUserDetailsManager(shabbir, nikhil);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(NoOpPasswordEncoder.getInstance());
+        return provider;
     }
 }
